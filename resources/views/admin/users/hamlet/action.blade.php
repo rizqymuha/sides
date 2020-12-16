90600b3f40af3d34d58d2f17f2e66af62ded791a
rizkygeek <ryzkysam@gmail.com>
2020-12-16 22:55:34 +0700

    add sweetalert

--- resources/views/admin/users/hamlet/action.blade.php
+++ resources/views/admin/users/hamlet/action.blade.php
@@ -1,13 +1,31 @@
 <a href="{{ route('admin.hamlet.user.edit', $model) }}" class="btn btn-info btn-sm">Edit</a>
 <button href="{{ route('admin.hamlet.user.destroy', $model) }}" id="delete" class="btn btn-danger btn-sm">Hapus</button>
 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
     $('button#delete').on('click', function(e) {
         e.preventDefault();
 
         var href = $(this).attr('href');
 
-        document.getElementById('delete-form').action = href;
-        document.getElementById('delete-form').submit();
+        Swal.fire({
+            title: 'Apakah kamu yakin?',
+            text: "Data yang telah terhapus tidak dapat dikembalikan!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Oke!'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById('delete-form').action = href;
+                document.getElementById('delete-form').submit();
+                Swal.fire(
+                'Terhapus!',
+                'Data berhasil dihapus',
+                'success'
+                )
+            }
+        })
     })
 </script>
